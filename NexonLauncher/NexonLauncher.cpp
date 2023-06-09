74bbfef1e31a137ee2e9730f55d8101f890efee5
David Joseph <davidjos3ph94@gmail.com>
2023-06-09 12:46:25 -0400

    remove redundant nexonlauncher file; remove junk code

---
+++ NexonLauncher/NexonLauncher.cpp
@@ -0,0 +1,16 @@
+#include "NexonLauncher.h"
+
+#include "../Patch/Patch.h"
+#include "../Patch/PatchManager.h"
+#include "../MapleStory/MapleStory.h"
+#include "../BlackCall/BlackCall.h"
+
+#define BUFF_SIZE 2048
+
+using namespace Patch;
+
+namespace NexonLauncher {
+	void InstallPatches()
+	{
+	}
+}
